19d8353ad481679f3462d0b8e324b12cb412a54e
Timothy Kirk <46324081+tkirk70@users.noreply.github.com>
2023-01-07 22:09:35 -0500

    Add pages.

--- sortable_draft.py
+++ sortable_draft.py
@@ -3,63 +3,66 @@ import pandas as pd
 import streamlit as st
 import streamlit.components.v1 as components
 
-# Try to increase width of layout
-st.set_page_config(layout="wide")
 
-# Create imgage variable for league logo
-components.html(
-    """
-    
-    <img class="NavSecondary__Logo__Img imageLoaded " src="https://g.espncdn.com/lm-static/ffl/images/ffl-shield-shield.svg" alt="Fantasy Football Home">
-    
-    
-    """,
-    width=159
-)
-# Load df from excel
-df = pd.read_excel('combined_newbees_draft.xlsx')
-# # Filter by year
-# years = df['year'].drop_duplicates()
-# make_choice = st.sidebar.selectbox('Select your year:', years)
-# filtered_df = df[df.loc['year'].is_in(make_choice)]
-# st.dataframe(filtered_df)
+def main_page():
 
-# Get rid of index
-# df.reset_index(drop=True, inplace=True)
+    # Try to increase width of layout
+    st.set_page_config(layout="wide")
 
-# Create header and subheader
-st.title('Sortable Newbees Drafts: 2013-2020')
-st.subheader(
-    'Use the dropdowns on the left to sort through different teams or years.')
+    # Create imgage variable for league logo
+    components.html(
+        """
+        
+        <img class="NavSecondary__Logo__Img imageLoaded " src="https://g.espncdn.com/lm-static/ffl/images/ffl-shield-shield.svg" alt="Fantasy Football Home">
+        
+        
+        """,
+        width=159
+    )
+    # Load df from excel
+    df = pd.read_excel('combined_newbees_draft.xlsx')
+    # # Filter by year
+    # years = df['year'].drop_duplicates()
+    # make_choice = st.sidebar.selectbox('Select your year:', years)
+    # filtered_df = df[df.loc['year'].is_in(make_choice)]
+    # st.dataframe(filtered_df)
 
-# Diplay the default dataframe.
-# st.dataframe(df, use_container_width=True)
+    # Get rid of index
+    # df.reset_index(drop=True, inplace=True)
 
-# Add radio button to choose between All or Individual
-st.sidebar.radio('Choose your own fantasy',
-                 ("All Newbees", "Individual Team"))
+    # Create header and subheader
+    st.title('Sortable Newbees Drafts: 2013-2020')
+    st.subheader(
+        'Use the dropdowns on the left to sort through different teams or years.')
 
-options = df['year'].drop_duplicates()
-options2 = df['round'].drop_duplicates()
-selected_options = st.sidebar.selectbox(
-    'Which year do you want relive?', options)
-selected_options2 = st.sidebar.selectbox('Which round do you want?', options2)
-st.write('Year: ', selected_options, 'Round: ', selected_options2)
-filtered_df = df[(df['year'] == selected_options) &
-                 (df['round'] == selected_options2)]
+    # Diplay the default dataframe.
+    # st.dataframe(df, use_container_width=True)
 
-# team_options = df['newbees_team'].unique().tolist()
-# selected_teams = st.sidebar.selectbox('Which team do you want?',team_options)
-# filtered_df2 = filtered_df[filtered_df["newbees_team"].isin(selected_teams)]
+    # Add radio button to choose between All or Individual
+    st.sidebar.radio('Choose your own fantasy',
+                     ("All Newbees", "Individual Team"))
 
-st.dataframe(filtered_df)
-# st.dataframe(filtered_df2)
+    options = df['year'].drop_duplicates()
+    options2 = df['round'].drop_duplicates()
+    selected_options = st.sidebar.selectbox(
+        'Which year do you want relive?', options)
+    selected_options2 = st.sidebar.selectbox(
+        'Which round do you want?', options2)
+    st.write('Year: ', selected_options, 'Round: ', selected_options2)
+    filtered_df = df[(df['year'] == selected_options) &
+                     (df['round'] == selected_options2)]
 
+    # team_options = df['newbees_team'].unique().tolist()
+    # selected_teams = st.sidebar.selectbox('Which team do you want?',team_options)
+    # filtered_df2 = filtered_df[filtered_df["newbees_team"].isin(selected_teams)]
 
-st.write('Where did you go wrong?')
+    st.dataframe(filtered_df)
+    # st.dataframe(filtered_df2)
 
-components.iframe(
-    "https://g.espncdn.com/lm-static/ffl/images/ffl-shield-shield.svg")
+    st.write('Where did you go wrong?')
+
+    components.iframe(
+        "https://g.espncdn.com/lm-static/ffl/images/ffl-shield-shield.svg")
 
 
 def page2():
