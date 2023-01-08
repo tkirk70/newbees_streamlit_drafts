47cb738164fcf20c31a6d930c2770e2536fc538e
Timothy Kirk <46324081+tkirk70@users.noreply.github.com>
2023-01-07 22:06:01 -0500

    Add pages.

--- sortable_draft.py
+++ sortable_draft.py
@@ -66,9 +66,10 @@ def page2():
     st.markdown("# Page 2 ❄️")
     st.sidebar.markdown("# Page 2 ❄️")
 
+
 page_names_to_funcs = {
     "Main Page": main_page,
-    "Page 2": page2
+    "Heat Map": page2
 }
 
 selected_page = st.sidebar.selectbox(
