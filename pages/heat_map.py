4e10bb9250cb07e1f31b7b1093159d8a0efd1c30
TDS <tds@tcg3pl.com>
2023-02-10 14:35:39 -0500

    Adding seaborn heatmap.

--- pages/heat_map.py
+++ pages/heat_map.py
@@ -5,35 +5,35 @@ import streamlit as st
 # Set the page wide
 st.set_page_config(layout="wide")
 
-# format precision for floats
-pd.set_option('display.float_format','{:.2f}'.format)
+# # format precision for floats
+# pd.set_option('display.float_format','{:.2f}'.format)
 
-st.markdown("# Heat Maps under construction 🔥")
-st.markdown("## Weekly Newbees Scoring - 2021")
-st.sidebar.markdown("# Heat Maps 🔥")
+# st.markdown("# Heat Maps under construction 🔥")
+# st.markdown("## Weekly Newbees Scoring - 2021")
+# st.sidebar.markdown("# Heat Maps 🔥")
 
-# figure out how to make this df without excel.
+# # figure out how to make this df without excel.
 
-df = pd.read_excel('season_2021.xlsx', sheet_name='combined_2021')
+# df = pd.read_excel('season_2021.xlsx', sheet_name='combined_2021')
 
-teams = df.columns.to_list()
+# teams = df.columns.to_list()
 
-weeks = df.Week.to_list()
+# weeks = df.Week.to_list()
 
-df.set_index(['Week'], inplace=True)
+# df.set_index(['Week'], inplace=True)
 
-# Python program to generate a heatmap
-# which displays the value in each cell
-# corresponding to the given dataframe
+# # Python program to generate a heatmap
+# # which displays the value in each cell
+# # corresponding to the given dataframe
 
-# import required libraries
+# # import required libraries
 
-# displaying dataframe as an heatmap
-# with diverging colourmap as virdis
-df_heat = df.style.background_gradient(cmap='viridis')\
-    .set_properties(**{'font-size': '11px'}).format(precision=2)
+# # displaying dataframe as an heatmap
+# # with diverging colourmap as virdis
+# df_heat = df.style.background_gradient(cmap='viridis')\
+#     .set_properties(**{'font-size': '11px'}).format(precision=2)
 
-st.dataframe(df_heat, use_container_width=True)
+# st.dataframe(df_heat, use_container_width=True)
 
 st.markdown("# Seaborn Heat Maps under construction 🔥")
 st.markdown("## Weekly Newbees Scoring - 2021")
