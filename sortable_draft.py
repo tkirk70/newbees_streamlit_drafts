f0ebf184cfdb5b0cfe11fe39faf256d98d92e899
TDS <tds@tcg3pl.com>
2023-02-10 14:47:15 -0500

    Adding seaborn heatmap.

--- sortable_draft.py
+++ sortable_draft.py
@@ -63,28 +63,28 @@ st.write('Where did you go wrong?')
 components.iframe(
     "https://g.espncdn.com/lm-static/ffl/images/ffl-shield-shield.svg")
 
-st.markdown("# Heat Maps under construction 🔥")
-st.sidebar.markdown("# Heat Maps 🔥")
+# st.markdown("# Heat Maps under construction 🔥")
+# st.sidebar.markdown("# Heat Maps 🔥")
 
-# figure out how to make this df without excel.
+# # figure out how to make this df without excel.
 
-df = pd.read_excel('season_2021.xlsx', sheet_name='combined_2021')
+# df = pd.read_excel('season_2021.xlsx', sheet_name='combined_2021')
 
-teams = df.columns.to_list()
+# teams = df.columns.to_list()
 
-weeks = df.Week.to_list()
+# weeks = df.Week.to_list()
 
-df.set_index(['Week'], inplace=True)
+# df.set_index(['Week'], inplace=True)
 
-# Python program to generate a heatmap
-# which displays the value in each cell
-# corresponding to the given dataframe
+# # Python program to generate a heatmap
+# # which displays the value in each cell
+# # corresponding to the given dataframe
 
-# import required libraries
+# # import required libraries
 
-# displaying dataframe as an heatmap
-# with diverging colourmap as virdis
-df_heat = df.style.background_gradient(cmap='viridis')\
-    .set_properties(**{'font-size': '11px'})
+# # displaying dataframe as an heatmap
+# # with diverging colourmap as virdis
+# df_heat = df.style.background_gradient(cmap='viridis')\
+#     .set_properties(**{'font-size': '11px'})
 
-st.dataframe(df_heat)
+# st.dataframe(df_heat)
