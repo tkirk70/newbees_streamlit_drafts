813d3fde4e10605f780f2ddd482b56da6300bbce
TDS <tds@tcg3pl.com>
2023-02-10 14:27:15 -0500

    Adding seaborn heatmap.

--- pages/heat_map.py
+++ pages/heat_map.py
@@ -31,3 +31,25 @@ df_heat = df.style.background_gradient(cmap='viridis')\
     .set_properties(**{'font-size': '11px'}).format(precision=2)
 
 st.dataframe(df_heat, use_container_width=True)
+
+st.markdown("# Seaborn Heat Maps under construction 🔥")
+st.markdown("## Weekly Newbees Scoring - 2021")
+
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+import seaborn as sns
+
+df = pd.read_excel('season_2021.xlsx')
+df = df.set_index('Week')
+
+fig, ax = plt.subplots(figsize=(16,8))
+sns.set()
+sns.heatmap(df, cmap='viridis', annot=True, fmt='.1f', linewidth=.7) #.set(title="Newbee's 2021 Weekly Scores")
+# plt.xticks(rotation=45)
+# sns.color_palette("viridis", as_cmap=True)
+ax.xaxis.tick_top()
+ax.set_title("Newbee's 2021 Weekly Scores", fontsize=17, pad=40)
+plt.show()
+
+# How to show this in streamlit?
+st.pyplot(fig)
